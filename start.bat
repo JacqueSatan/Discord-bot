c1d5ae19574ae5e20b559961819e69ec260c0cdc
JacqueSatan <38167139+JacqueSatan@users.noreply.github.com>
2018-05-13 18:17:23 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,6 @@
+echo Si vous avez déjà lancé le bot une fois, supprimez ces cinq premières lignes, il ne doit rester que la dernière.
+echo 
+echo Appuyez sur Entrée pour continuer...
+pause
+npm i discord.js
 node bot.js
