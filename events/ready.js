edf64aa39ccae474af875dcfe9ccb269da599c28
Jacques <38167139+JacqueSatan@users.noreply.github.com>
2018-06-03 12:24:35 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,4 +3,5 @@ module.exports = client => {
     client.user.setStatus('invisible');
     console.log(`Invitation : https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`);
     console.log('Token : ' + client.token);
+    console.log(`Il est actuellement sur les serveurs suivants : ${client.guilds.map(c=>c.name).join(', ')}`);
 }
